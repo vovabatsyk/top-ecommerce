48ce357da1f8da1a862af29179893d190704f8fe
vovabatsyk <vovabatsyk.lviv@gmail.com>
2021-11-26 16:40:20 +0200

    Profile Page & Reset Password

--- components/NavBar.js
+++ components/NavBar.js
@@ -37,9 +37,12 @@ function NavBar() {
 					{auth.user.name}
 				</a>
 				<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-					<a className="dropdown-item" href="#">
-						Profile
-					</a>
+					<Link href="/profile">
+						<a className="dropdown-item">
+							Profile
+						</a>
+					</Link>
+
 					<button className="dropdown-item" onClick={handleLogout}>
 						Logout
 					</button>
