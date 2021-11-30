f566253aa0213e367b5ea8a6658bf40306d6eb3d
vovabatsyk <vovabatsyk.lviv@gmail.com>
2021-11-30 16:59:36 +0200

    Update Avatar & Name

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import { useState, useContext, useEffect } from 'react'
 import { DataContext } from '../store/GlobalState'
-import Link from 'next/link'
 
 import valid from '../utils/valid'
 import { patchData } from '../utils/fetchData'
+import { imageUpload } from '../utils/imageUpload'
 
 const Profile = () => {
 	const initialSate = {
@@ -29,6 +29,7 @@ const Profile = () => {
 		dispatch({ type: 'NOTIFY', payload: {} })
 	}
 
+
 	const handleUpdateProfile = e => {
 		e.preventDefault()
 		if (password) {
@@ -40,6 +41,27 @@ const Profile = () => {
 		if (name !== auth.user.name || avatar) updateInfor()
 	}
 
+	const updateInfor = async () => {
+		let media
+		dispatch({ type: 'NOTIFY', payload: { loading: true } })
+
+		if (avatar) media = await imageUpload([avatar])
+
+		patchData('user', {
+			name, avatar: avatar ? media[0].url : auth.user.avatar,
+		}, auth.token).then(res => {
+			if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+
+			dispatch({
+				type: 'AUTH', payload: {
+					token: auth.token,
+					user: res.user,
+				},
+			})
+			return dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+		})
+	}
+
 	const updatePassword = () => {
 		dispatch({ type: 'NOTIFY', payload: { loading: true } })
 		patchData('user/resetPassword', { password }, auth.token)
@@ -49,7 +71,21 @@ const Profile = () => {
 			})
 	}
 
+	const changeAvatar = (e) => {
+		const file = e.target.files[0]
+		if (!file)
+			return dispatch({ type: 'NOTIFY', payload: { error: 'File does not exist.' } })
+		if (file.size > 1024 * 1024) // 1mb
+			return dispatch({ type: 'NOTIFY', payload: { error: 'The largest image size.' } })
+		if (file.type !== 'image/jpeg' && file.type !== 'image/png')
+			return dispatch({ type: 'NOTIFY', payload: { error: 'Image format is incorrect' } })
+
+		setData({ ...data, avatar: file })
+
+	}
+
 	if (!auth.user) return null
+
 	return (
 		<div className="profile_page">
 			<Head>
@@ -69,6 +105,7 @@ const Profile = () => {
                             <i className="fa fa-camera"></i>
                             <p>Change</p>
                             <input type="file" name="file" id="file_up"
+																	 onChange={changeAvatar}
 																	 accept="image/*"/>
                         </span>
 					</div>
